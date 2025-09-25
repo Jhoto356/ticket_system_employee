b457c3d3f628366a9882da640f9d8742b02f930a
Jhoto356 <jhotocastro356@gmail.com>
2025-09-25 15:41:22 -0500

    feature: Remove and rename files.

--- app/src/main/java/com/example/ticket_system_employee/core/di/RepositoryFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ticket_system_employee.core.di
-
-import android.content.Context
-import com.example.ticket_system_employee.data.repositoryImp.LoginRepositoryImp
-
-object RepositoryFactory {
-    fun loginRepository(context: Context) = LoginRepositoryImp.getInstance(context)
-
-}
--- app/src/main/java/com/example/ticket_system_employee/core/di/UseCasesFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ticket_system_employee.core.di
-
-import com.example.ticket_system_employee.domain.repository.login.LoginRepository
-import com.example.ticket_system_employee.domain.useCases.login.LoginUseCases
-
-object UseCasesFactory {
-    fun loginUseCases(loginRepository: LoginRepository) = LoginUseCases(loginRepository)
-}

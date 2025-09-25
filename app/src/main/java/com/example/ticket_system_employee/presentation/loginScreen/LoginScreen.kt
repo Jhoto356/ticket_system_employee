19736880d2749d47ccb9d53d4bf7e0702ed5afea
Jhoto356 <jhotocastro356@gmail.com>
2025-09-24 20:38:26 -0500

    feature: Text Styles add title

--- app/src/main/java/com/example/ticket_system_employee/presentation/loginScreen/LoginScreen.kt
+++ app/src/main/java/com/example/ticket_system_employee/presentation/loginScreen/LoginScreen.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.ticket_system_employee.core.di.Module
 import com.example.ticket_system_employee.core.navigation.StartupNavHost
 import com.example.ticket_system_employee.presentation.theme.TicketSystemEmpleyeeTheme
+import com.example.ticket_system_employee.presentation.theme.White
 
 class LoginScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +34,8 @@ class LoginScreen : ComponentActivity() {
 
 @Composable
 fun LoginMain(navController: NavHostController) {
+    Box(modifier = Modifier.fillMaxSize().systemBarsPadding().imePadding().background(White)) {
+
+    }
 
 }
---
+++ app/src/main/java/com/example/ticket_system_employee/presentation/loginScreen/LoginVM.kt
@@ -0,0 +1,9 @@
+package com.example.ticket_system_employee.presentation.loginScreen
+
+import androidx.lifecycle.ViewModel
+
+class LoginVM: ViewModel() {
+    init {
+
+    }
+}

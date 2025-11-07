90d8bcc4d48e71d3df744b71692e7b62b2cf5dcb
Jhoto356 <jhotocastro356@gmail.com>
2025-11-07 18:35:29 -0500

    feature: Finish

--- app/src/main/java/com/example/ticket_system_employee/core/db/entities/TicketEntity.kt
+++ app/src/main/java/com/example/ticket_system_employee/core/db/entities/TicketEntity.kt
@@ -1,6 +1,7 @@
 package com.example.ticket_system_employee.core.db.entities
 
 data class TicketEntity(
+    val id: Long = 0,
     val requestType: String,
     val area: String,
     val description: String,
